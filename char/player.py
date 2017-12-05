1440452e593bc1d1ecde81b00b2aad5a03d4c7da
omyjosh <tronnytsunami@gmail.com>
2017-12-05 01:13:31 -0500

    Refine how player is displayed

--- char/player.py
+++ char/player.py
@@ -1,8 +1,12 @@
 import pygame
 from functions import load_image
+from pygame.locals import *
 
 class Puppy(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image, self.rect = load_image('assets\pup.png', -1)
+		self.image, self.rect = load_image('pup.png', -1)
+		screen = pygame.display.get_surface()
+		self.area = screen.get_rect()
+		self.rect.topleft = 10, 50
 
