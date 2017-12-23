9c966c079c0a293ed823c8986541d53d939388e3
Peter Travers <traverspw@gmail.com>
2017-12-23 15:19:50 -0500

    Refactor project structure to allow for test package within root project directory

--- __main__.py
+++ __main__.py
@@ -1,44 +1,5 @@
-import pygame
-from map.map import Map
-from characters.puppy import Puppy
-from pygame.locals import *
-
-height = 600
-width = 400
-
-
-def run_game():
-    try:
-        pygame.init()
-
-        screen = pygame.display.set_mode((height, width))
-        pygame.display.set_caption('The Saddest Pup')
-
-        screen.fill((42, 42, 42))
-
-        game_map = Map(size=14, screen=screen)
-
-        puppy = Puppy()
-
-        game_map.draw_map()
-
-        player = pygame.sprite.RenderPlain(puppy)
-
-        player.draw(screen)
-
-        pygame.display.update()
-
-        game_running = True
-
-        while game_running:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    game_running = False
-                    break
-
-    finally:
-        pygame.quit()
-
+from game.starman import StarMan
 
 if __name__ == "__main__":
-    run_game()
+    game = StarMan()
+    game.run_game()
---
+++ game/starman.py
@@ -0,0 +1,45 @@
+import pygame
+
+from pygame.locals import QUIT
+
+from game.map.map import Map
+
+
+class StarMan:
+
+    def __init__(self):
+        self.height = 600
+        self.width = 400
+
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((self.height, self.width))
+
+        self.screen.fill((42, 42, 42))
+
+        self.game_map = Map(size=14)
+
+    def run_game(self):
+        try:
+            self.draw_map()
+
+            game_running = True
+
+            while game_running:
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        game_running = False
+                        break
+
+        finally:
+            pygame.quit()
+
+    def draw_map(self):
+        for row in self.game_map.matrix:
+            for map_rect in row:
+
+                pygame.draw.rect(
+                    self.screen,
+                    self.game_map.available_square_color,
+                    map_rect
+                )
